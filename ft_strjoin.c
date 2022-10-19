f9cceae55b85bff213d053379cd8902ed094f713
gcssilva <gustavocssilva1997@gmail.com>
2022-10-19 09:29:11 +0100

    add minor change

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: gsilva <gsilva@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 09:33:32 by gsilva            #+#    #+#             */
-/*   Updated: 2022/10/18 09:35:16 by gsilva           ###   ########.fr       */
+/*   Updated: 2022/10/19 09:13:25 by gsilva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	i = 0;
 	str = (char *)malloc(ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1);
+	if (!str)
+		return (NULL);
 	while (s1 && *s1)
 		str[i++] = *s1++;
-	while (s1 && *s2)
+	while (s2 && *s2)
 		str[i++] = *s2++;
 	str[i] = 0;
 	return (str);
